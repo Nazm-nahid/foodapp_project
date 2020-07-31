1b45e4a1703e5764467d92731aab944c62a0964f
Nahid <nazm.nahid@gmail.com>
2020-07-31 15:36:42 +0600

    push done next next

--- Rider/views.py
+++ Rider/views.py
@@ -1,27 +1,40 @@
 from django.shortcuts import render
 from .models import Rider,Store
 from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
+#from django.contrib.auth.models import User
 
 # Create your views here.
+@login_required(login_url="/user/login/")
 def riderview(request):
     template = 'Rider/riderhome.html'
     riders = Rider.objects.all()
+    if (request.user.userprofile.check == 'rider'):
+        return render(request, template, {"riders": riders})
+    else:
+        return HttpResponse('Illigal Action!!!')
 
-    return render(request, template, {"riders": riders})
-
+@login_required(login_url="/user/login/")
 def storeview(request):
     template = 'Rider/storehome.html'
     list_items = Store.objects.all()
+    if (request.user.userprofile.check == 'store'):
+        return render(request, template, {"items": list_items})
+    else:
+        return HttpResponse('Illigal Action!!!')
 
-    return render(request, template, {"items": list_items})
-
-
+@login_required(login_url="/user/login/")
 def accept(request):
-    if request.method == 'POST':
-        return HttpResponse('Accepted')
+    if (request.user.userprofile.check == 'rider'):
+        if request.method == 'POST':
+            return HttpResponse('Accepted')
+    else:
+        return HttpResponse('Illigal Action!!!')
 
-
+@login_required(login_url="/user/login/")
 def payment(request):
-    if request.method == 'POST':
-        return HttpResponse('Payment Done')
+    if (request.user.userprofile.check == 'rider'):
+        if request.method == 'POST':
+            return HttpResponse('Payment Done')
+    else:
+        return HttpResponse('Illigal Action!!!')
