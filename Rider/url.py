1deb3111ace705b406935e553c88c9a490e38bc7
Nazmul Hossain Nahid <54554725+Nazm-nahid@users.noreply.github.com>
2020-07-31 12:36:01 +0600

    Update url.py

--- Rider/url.py
+++ Rider/url.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from .import views
 
 urlpatterns =[
+    url(r'^riderhome/$',views.riderview, name='riderhome'),
+    url(r'^storehome/$',views.storeview, name='storehome'),
     url(r'^ac/$',views.accept, name='accept'),
     url(r'^pay/$',views.payment, name='payment'),
 ]
