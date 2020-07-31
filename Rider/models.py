29cddd98dce4bb3b1930e47a73d87f387d6f6a60
Nazmul Hossain Nahid <54554725+Nazm-nahid@users.noreply.github.com>
2020-07-31 12:36:37 +0600

    Update models.py

--- Rider/models.py
+++ Rider/models.py
@@ -6,10 +6,9 @@ class Rider(models.Model):
     time= models.TimeField( )
     rider_ac= models.CharField(max_length=200)
     def __str__(self):
-        return self.id
+        return self.rider_ac
 class Store(models.Model):
-    id = models.ForeignKey(Rider, on_delete=models.CASCADE)
-    resturent=models.CharField(max_length=200)
+    resturent = models.CharField(max_length=200)
     order_item= models.CharField(max_length=50)
     time= models.TimeField( )
     payment= models.CharField(max_length=3)
